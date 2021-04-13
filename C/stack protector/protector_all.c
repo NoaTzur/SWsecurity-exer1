415517fbdf21caead51f4c51b4fb8103d5114ebc
NoaTzur <noatzur9@gmail.com>
2021-04-13 23:55:28 +0300

    end of exer

---
+++ C/stack protector/protector_all.c
@@ -0,0 +1,9 @@
+#include <string.h>
+
+
+int main(void)
+{
+    char buffer[3];
+    strcpy(buffer, "dorandnoa");
+    return buffer[0];
+}
---
+++ C/stack protector/strong_protector.c
@@ -0,0 +1,16 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+int main()
+{
+  char arr[100];
+  int  i;
+
+  for (i = 0; i < 150; i++) {
+    arr[i] = (char)94;
+    }
+    
+    
+  return 0;
+}
+
